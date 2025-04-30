52f3134b8a36d7f59acc7b477208afbf1a7028fb
7kharo <7kharo@gmail.com>
2025-04-30 18:43:13 +0300

    create documentation 0.2
    upd README.md

---
+++ src/types/index.ts
@@ -0,0 +1,38 @@
+type ProductType = 'софт-скилл' | 'другое' | 'дополнительное' | 'кнопка' | 'хард-скилл';
+type PaymentType = 'онлайн' | 'при получении'; 
+
+ export interface IProduct {
+    id: string;
+    title: string;
+    description: string;
+    category: ProductType;
+    price: number | null;
+    image: string;
+}
+
+export interface IForm {
+    payment: PaymentType;
+    address: string;
+    email: string;
+    tel: string;
+    clearForm(): void;
+}
+ 
+export interface IProductList {
+    products: TProductMain[];
+}
+
+export interface IBasket {
+    productsBasket: TProductBasket[];
+    addProduct(product: TProductBasket): void;
+    deleteProduct(productId: string):void;
+    clearBasket():void;
+}
+
+export type TProductMain = Pick<IProduct, 'id' | 'title' | 'image' | 'category' | 'price'>;
+
+export type TProductBasket = Pick<IProduct, 'id' | 'title' | 'price'>;
+
+export type TFormPayment = Pick<IForm, 'address' | 'payment'>;
+
+export type TFormContacts = Pick<IForm, 'email' | 'tel'>;
